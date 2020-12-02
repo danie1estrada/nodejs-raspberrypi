140dc28a409aaa56f186e6368fae48ec7ea1a2d7
Daniel Estrada <isc.daniel.estrada@gmail.com>
2020-12-02 13:06:55 -0600

    Read distance pt 1

--- distance.js
+++ distance.js
@@ -1,13 +1,33 @@
-const Gpio = require('onoff').Gpio
+console.log('Aaah x2'))
+const Gpio = require('pigpio').Gpio
 
-const trigger = new Gpio(18, 'out')
-const echo = new Gpio(15, 'in')
+const MICRO_SECONDS_PER_CM = 1e6 / 34321
+
+const trigger = new Gpio(18, { mode: Gpio.OUTPUT })
+const echo = new Gpio(15, { mode: Gpio.INPUT, alert: true })
+
+trigger.digitalWrite(0)
+
+const readTemperature = () => {
+	let startTick;
+
+	echo.on('alert', (level, tick) => {
+		console.log('Aaah)
+		if (level == 1) {
+			startTick = tick
+		} else {
+			const endTick = tick
+			const diff = (endTick >> 0) - (startTick >> 0)
+			console.log(diff / 2 / MICROSECONDS_PER_CM)
+		}
+	})
+}
+
+console.log('Aaaah x2')
+readTemperature()
 
 setInterval(() => {
-	
+	trigger.trigger(10, 1)
 }, 1000)
 
-process.on('SIGINT', () => {
-	trigger.unexport()
-	echo.unexport()
-})
+
--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-const express = require('express')
+/*const express = require('express')
 const app = express()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http, { cors: { origin: '*' }})
@@ -37,4 +37,36 @@ http.listen(3000, () => {
 process.on('SIGINT', () => {
 	led.writreSync(0)
 	led.unexport()
-})
+})*/
+
+const Gpio = require('pigpio').Gpio
+
+const MICRO_SECONDS_PER_CM = 1e6 / 34321
+
+const trigger = new Gpio(18, { mode: Gpio.OUTPUT })
+const echo = new Gpio(15, { mode: Gpio.INPUT, alert: true })
+
+trigger.digitalWrite(0)
+
+const readTemperature = () => {
+	let startTick;
+
+	echo.on('alert', (level, tick) => {
+		if (level == 1) {
+			startTick = tick
+		} else {
+			const endTick = tick
+			const diff = (endTick >> 0) - (startTick >> 0)
+			const distance = diff / 2 / MICRO_SECONDS_PER_CM
+			console.log(distance)
+		}
+	})
+}
+
+readTemperature()
+
+setInterval(() => {
+	trigger.trigger(10, 1)
+}, 1000)
+
+
