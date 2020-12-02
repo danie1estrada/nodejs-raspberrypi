6be68450a826159e912261ea3565cd8e91b8429d
Daniel Estrada <isc.daniel.estrada@gmail.com>
2020-12-02 13:13:07 -0600

    ...

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
-/*const express = require('express')
+const Gpio = require('pigpio').Gpio
+const express = require('express')
 const app = express()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http, { cors: { origin: '*' }})
 
-const Gpio = require('onoff').Gpio
+const MICRO_SECONDS_PER_CM = 1e6 / 34321
 
-const led = new Gpio(14, 'out')
+const trigger = new Gpio(18, { mode: Gpio.OUTPUT })
+const echo = new Gpio(15, { mode: Gpio.INPUT, alert: true })
+
+trigger.digitalWrite(0)
 
 app.get('/', (req, res) => {
 	res.send('Server runnung...')
@@ -13,6 +17,28 @@ app.get('/', (req, res) => {
 
 io.on('connection', socket => {
 	console.log('New connection')
+
+	const readTemperature = () => {
+		let startTick;
+	
+		echo.on('alert', (level, tick) => {
+			if (level == 1) {
+				startTick = tick
+			} else {
+				const endTick = tick
+				const diff = (endTick >> 0) - (startTick >> 0)
+				const distance = diff / 2 / MICRO_SECONDS_PER_CM
+				
+				socket.emit('distance', { distance })
+				console.log(distance)
+			}
+		})
+	}
+
+	readTemperature()
+	setInterval(() => {
+		trigger.trigger(10, 1)
+	}, 1000)
 
 	socket.on('add note', note => {
 			
@@ -28,45 +54,9 @@ io.on('connection', socket => {
 		console.log('Turn off led')
 		led.writeSync(0)
 	})
+
 })
 
 http.listen(3000, () => {
 	console.log('Server running...')
 })
-
-process.on('SIGINT', () => {
-	led.writreSync(0)
-	led.unexport()
-})*/
-
-const Gpio = require('pigpio').Gpio
-
-const MICRO_SECONDS_PER_CM = 1e6 / 34321
-
-const trigger = new Gpio(18, { mode: Gpio.OUTPUT })
-const echo = new Gpio(15, { mode: Gpio.INPUT, alert: true })
-
-trigger.digitalWrite(0)
-
-const readTemperature = () => {
-	let startTick;
-
-	echo.on('alert', (level, tick) => {
-		if (level == 1) {
-			startTick = tick
-		} else {
-			const endTick = tick
-			const diff = (endTick >> 0) - (startTick >> 0)
-			const distance = diff / 2 / MICRO_SECONDS_PER_CM
-			console.log(distance)
-		}
-	})
-}
-
-readTemperature()
-
-setInterval(() => {
-	trigger.trigger(10, 1)
-}, 1000)
-
-
