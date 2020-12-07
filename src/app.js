15dc33e819eb69d6dd5265a862ba2327aa0c52db
Daniel Estrada <isc.daniel.estrada@gmail.com>
2020-12-06 21:43:57 -0600

    ...

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+const dht = require('node-dht-sensor')
 const Gpio = require('pigpio').Gpio
 const express = require('express')
 const app = express()
@@ -6,38 +7,49 @@ const io = require('socket.io')(http, { cors: { origin: '*' }})
 
 const MICRO_SECONDS_PER_CM = 1e6 / 34321
 
+const led = new Gpio(14, { mode: Gpio.OUTPUT })
 const trigger = new Gpio(18, { mode: Gpio.OUTPUT })
 const echo = new Gpio(15, { mode: Gpio.INPUT, alert: true })
 
 trigger.digitalWrite(0)
+led.digitalWrite(0)
 
 app.get('/', (req, res) => {
 	res.send('Server runnung...')
 })
 
 io.on('connection', socket => {
-	console.log('New connection')
+	console.log(`New client (${socket.id})`)
 
-	const readTemperature = () => {
+	const readDistance = () => {
 		let startTick;
 	
 		echo.on('alert', (level, tick) => {
 			if (level == 1) {
 				startTick = tick
 			} else {
-				const endTick = tick
-				const diff = (endTick >> 0) - (startTick >> 0)
+				const diff = (tick >> 0) - (startTick >> 0)
 				const distance = diff / 2 / MICRO_SECONDS_PER_CM
-				
+
 				socket.emit('distance', { distance })
 			}
 		})
 	}
-	readTemperature()
+	readDistance()
 	
 	setInterval(() => {
 		trigger.trigger(10, 1)
 	}, 1000)
+
+	setInterval(() => {
+		dht.read(11, 23, (err, temperature, humidity) => {
+			if (!err) socket.emit('temperature', { temperature, humidity })
+		})
+	}, 5000)
+
+	socket.on('toggle light', value => {
+		led.digitalWrite(value)
+	})
 
 	socket.on('add note', note => {
 		console.log(note);
@@ -45,18 +57,19 @@ io.on('connection', socket => {
 	})
 
 	socket.on('delete note', noteId => {
+		console.log(`Note deleted: (${noteId})`)
 		socket.broadcast.emit('note deleted', noteId)
 	})
 
 	socket.on('present image', imageUrl => {
+		console.log(`Image presented: (${imageUrl})`)
 		socket.broadcast.emit('present image', imageUrl)
 	})
 
 	socket.on('disconnect', () => {
-		console.log('Client disconnected')
+		console.log(`Client disconnected (${socket.id})`)
 	})
 })
-
 
 http.listen(3000, () => {
 	console.log('Server running...')
